afd63e726c86e2229d396f0566e47f4633ed9bc6
ElbethelSolomon5149 <solomonelbethel@gmail.com>
2023-04-25 18:06:14 -0700

    lenstr

---
+++ lenstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - calculate length of str
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(const char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] = 0; i++)
+		return(i);
+}
